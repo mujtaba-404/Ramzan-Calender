607be63eef53031e5efb1c476bced7210a5f1c4d
Muhammad Mujtaba <contact.muhammadmujtaba@gmail.com>
2025-03-06 22:16:23 +0500

    ..

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className= "bg-green-900" >
       <marquee scrollamount="20">
-  <h2 className='text-white text-5xl mt-3'>RAMZAN MUBARAK 2024</h2>
+  <h2 className='text-white text-5xl mt-3'>RAMZAN KAREEM 2025</h2>
 </marquee>
 
     </div>
